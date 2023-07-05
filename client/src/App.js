3d91a6fc8b29c8113a8ff9c36b3b00f246e87eb6
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-07-05 16:53:12 +0530

    implementing navabar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from 'Scenes/HomePage';
+import LoginPage from 'Scenes/LoginPage';
+import ProfilePage from 'Scenes/ProfilePage';
+// import NavBar from 'Scenes/NavBar';
+import { useMemo } from 'react';
+import { useSelector } from 'react-redux'
+import { CssBaseline,ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material';
+import { themeSettings } from 'theme';
 
 const App = () => {
+  const mode=useSelector((state)=>state.mode);
+  const theme=useMemo(()=>createTheme(themeSettings(mode)),[mode]);
+
   return (
-    <div>App</div>
+    <div className='app'>
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline/>
+        <Routes>
+          <Route path='/' element={<LoginPage/>}/>
+          <Route path='/home' element={<HomePage/>}/>
+          <Route path='/profile/:userId' element={<ProfilePage/>}/>
+        </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
+    </div>
   )
 }
 
