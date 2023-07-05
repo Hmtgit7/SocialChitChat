a3f088bfe5f6d1c86bcad922d7167db0c864f3a3
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-07-05 16:52:06 +0530

    simple structuring component for test

---
+++ client/src/Scenes/HomePage/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from 'Scenes/NavBar';
+
+const HomePage = () => {
+  return (
+
+    <div>
+      <NavBar/>
+      <h1>HomePage</h1>
+    </div>
+  )
+}
+
+export default HomePage;
